dd8b46bfb7500c58dbda463c433c196787d7a331
Jan Šipr <sipr.jan@gmail.com>
2022-01-18 17:35:16 +0100

    Fix fonts and symbols in the power menu script

--- powermenu.sh
+++ powermenu.sh
@@ -4,15 +4,15 @@ rofi_command="rofi"
 
 
 # Options
-shutdown=" Shutdonw"
-reboot=" Reboot"
-lock=" Lock"
-suspend=" Suspend"
+shutdown="⏻  Shutdonw"
+reboot="↺  Reboot"
+lock="🔒 Lock"
+suspend="💤 Suspend"
 
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock\n$suspend\n"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -dmenu -font "Iosevka Nerd Font 12" -selected-row 2)"
 case $chosen in
     $shutdown)
         systemctl poweroff
