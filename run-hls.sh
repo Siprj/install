b5fc134612cb6df22a98c5ea75def0a29f7fbc05
Jan Sipr <sipr.jan@gmail.com>
2022-11-14 16:03:13 +0100

    Messed up capitalization

--- run-hls.sh
+++ run-hls.sh
@@ -3,7 +3,7 @@ set -e
 
 DIR_NAME=$(basename "${PWD}")
 
-if [[ "${DIR_NAME}" == "Kontrakcja" ]]; then
+if [[ "${DIR_NAME}" == "kontrakcja" ]]; then
     docker compose "-f" "docker/docker-compose.yml" "exec" "devel" "haskell-language-server-wrapper" $@
 else
     haskell-language-server-wrapper $@
