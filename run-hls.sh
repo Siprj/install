e08dd4bccdee3906679a3cfd2db10cd312e82fcc
Jan Sipr <sipr.jan@gmail.com>
2023-03-10 13:19:26 +0100

    Add journey-service hls support

--- run-hls.sh
+++ run-hls.sh
@@ -5,6 +5,8 @@ DIR_NAME=$(basename "${PWD}")
 
 if [[ "${DIR_NAME}" == "kontrakcja" ]]; then
     docker compose "-f" "docker/docker-compose.yml" "exec" "devel" "haskell-language-server-wrapper" $@
+elif [[ "${DIR_NAME}" == "journey-service" ]]; then
+    docker compose "exec" "devel" "haskell-language-server-wrapper" $@
 else
     haskell-language-server-wrapper $@
 fi
