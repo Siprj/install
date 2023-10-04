2ff617bcaa5561827bd0acff448cc2f46d5cfff6
Jan Sipr <sipr.jan@gmail.com>
2023-10-04 15:51:27 +0200

    Waybar configuration

--- install.sh
+++ install.sh
@@ -177,6 +177,21 @@ if [ ! -L "${HOME}/.config/mako/config" ]; then
     ln -s "${PROG_DIR}/mako.config" "${HOME}/.config/mako/config"
 fi
 
+if [ ! -L "${HOME}/.config/waybar/config.jsonc" ]; then
+    rm -R -f "${HOME}/.config/waybar/config.jsonc"
+    ln -s "${PROG_DIR}/waybar.jsonc" "${HOME}/.config/waybar/config.jsonc"
+fi
+
+if [ ! -L "${HOME}/.config/waybar/style.css" ]; then
+    rm -R -f "${HOME}/.config/waybar/style.css"
+    ln -s "${PROG_DIR}/waybar.css" "${HOME}/.config/waybar/style.css"
+fi
+
+if [ ! -L "${HOME}/.config/waybar/catppuccin-mocha.css" ]; then
+    rm -R -f "${HOME}/.config/waybar/catppuccin-mocha.css"
+    ln -s "${PROG_DIR}/waybar-catppuccin-mocha.css" "${HOME}/.config/waybar/catppuccin-mocha.css"
+fi
+
 # Set firefox catppuccin flamingo theme
 firefox https://color.firefox.com/?theme=XQAAAAJHBAAAAAAAAABBqYhm849SCicxcUcPX38oKRicm6da8pFtMcajvXaAE3RJ0F_F447xQs-L1kFlGgDKq4IIvWciiy4upusW7OvXIRinrLrwLvjXB37kvhN5ElayHo02fx3o8RrDShIhRpNiQMOdww5V2sCMLAfehhp8u7kT4nh31-_5sD_P8FhlfX9Sdj_brd9hzw5NA_jx4peTGmoiUcikCHxa8Sm8bylvXElo3HHzylyv8f7R7gwkSEe8Mkq_ERB00vhRYSdLVEI7OR2j9y8UtYJhXmmHxXtQ2a2q0wDt9h-Dv7L5NTOL6rXow07mQCwsiafOlEKwLdkeAd2DoxJ1_Pu_amXOiUhOKrOw2DBrS-cIjSXWu9in58J8EBSEno0b4K2apcsY4mww6HdBAXjQjS7PBl1Eoli3qcNvy3o0v-yq9guO7ozjOWAFY-rVMCACPIWLr-pEBHErXolnftBIiOuC_k1brGAscZ579rDSHW_Bf9KewXOw3subWzfX0sPqI5eJLXKKLKfJEuPnm7z6IlEkCi__KG8k0-VIsE0lvbgk_dPXNsl8__ihao0
 
@@ -225,7 +240,6 @@ if [ ! -L "${HOME}/.wezterm.lua" ]; then
 fi
 
 mkdir -p "${HOME}/.zsh"
-cp "${PROG_DIR}/zshrc" "${HOME}/.zshrc"
 if [ ! -L "${HOME}/.zshrc" ]; then
     rm -R -f "${HOME}/.zshrc"
     ln -s "${PROG_DIR}/zshrc" "${HOME}/.zshrc"
