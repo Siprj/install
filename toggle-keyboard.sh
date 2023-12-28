c29f26e8f8c8dca531c66bba087861b0da61d163
Jan Sipr <sipr.jan@gmail.com>
2023-12-28 14:33:56 +0100

    keyboard switching in sway

--- toggle-keyboard.sh
+++ toggle-keyboard.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-LANG=$(setxkbmap -query | grep "layout:" | sed 's/layout: *\(.*\)$/\1/')
-
-if [ ${LANG} == "us" ]; then
-    setxkbmap cz qwerty
-else
-    setxkbmap us
-fi
+swaymsg input type:keyboard xkb_switch_layout next
