0c171098079a3849017e8bf85ae611c1e1a42423
Jan Sipr <sipr.jan@gmail.com>
2024-02-22 17:11:39 +0100

    Trying copilot

--- nvim/init.lua
+++ nvim/init.lua
@@ -1156,6 +1156,57 @@ local markdown_preview = {
 }
 
 
+local copilot = {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
+  end,
+}
+
 local plugins = {
   catppuccin,
   tokyonight,
@@ -1179,7 +1230,8 @@ local plugins = {
   zk_nvim,
   fugitive,
   fidget,
-  markdown_preview
+  markdown_preview,
+  copilot
 }
 
 require("lazy").setup(plugins)
