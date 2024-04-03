fec0758a8513506584bfbde62bac97ef6412d698
Jan Sipr <sipr.jan@gmail.com>
2024-04-03 21:30:28 +0200

    Run the this-is-the-sway as service

--- install.sh
+++ install.sh
@@ -205,7 +205,8 @@ systemctl --user disable swayrd
 
 # Get this-is-the-sway workspace switching utility
 curl -L --proto '=https'  https://github.com/Siprj/this-is-the-sway/releases/download/v0.0.1/this-is-the-sway.zip -o /tmp/this-is-the-sway.zip
-(cd ~/.local/bin/ && unzip -o /tmp/this-is-the-sway.zip && chmod +x this-is-the-sway)
+(cd /usr/local/bin/ && sudo unzip -o /tmp/this-is-the-sway.zip && sudo chmod +x this-is-the-sway)
+sudo cp this-is-the-sway.service /usr/lib/systemd/user/
 
 mkdir -p ~/.config/mako/
 if [ ! -L "${HOME}/.config/mako/config" ]; then
